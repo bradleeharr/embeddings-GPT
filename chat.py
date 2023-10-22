cdd16dde4f1a0d905e4b63a822d1fb4fe0511de6
Bradlee Harrison <bradlee.harrison22@gmail.com>
2023-10-22 16:46:35 -0500

    Previous context fully working. Chat is stable

--- chat.py
+++ chat.py
@@ -1,6 +1,7 @@
 import openai
 import numpy as np
 import pandas as pd
+import tiktoken
 import time
 
 from get_element_data import load_config
@@ -10,11 +11,8 @@ from flask_socketio import emit
 config = load_config("credentials.json")
 openai.api_key = config["openai-api-key"]
 
-messages = [
-    {"role": "system", "content": "You're a terse GNU Radio assistant..."}]
-datafile_path = "processed/embeddings.csv"
-
 # read the datafile
+datafile_path = "processed/embeddings.csv"
 df = pd.read_csv(datafile_path)
 df["embeddings"] = df.embeddings.apply(eval).apply(np.array)
 print("Loaded GitHub Embeddings")
@@ -22,18 +20,22 @@ print("Loaded GitHub Embeddings")
 
 def count_tokens(text):
     """Count the number of tokens in a text string using OpenAI's tokenizer."""
-    return len(openai.tokenize(text))
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+    print(text)
+    print(len(tokenizer.encode(text)))
+    return len(tokenizer.encode(text))
 
 
-def manage_context_length(messages, new_message_content):
+def manage_context_length(messages):
     """Ensure the total tokens in the context (including the new message) don't exceed 4000."""
     total_tokens = sum(count_tokens(msg["content"]) for msg in messages)
-    total_tokens += count_tokens(new_message_content)
-
+    print("Total Tokens:", total_tokens)
     # While we are over the token limit, remove the earliest messages
     while total_tokens > 4000:
         removed_message = messages.pop(0)
+        print("Removed message: ", removed_message)
         total_tokens -= count_tokens(removed_message["content"])
+        print("Total tokens: ", total_tokens)
 
     return messages
 
@@ -51,37 +53,39 @@ def search_justice(df, search, threshold=0.8):
     return highScores
 
 
+messages = [{"role": "system", "content": "You're a GNU Radio assistant..."}]
+
+
 def handle_message(text):
+    """handle search and chat when a new message is input by a user"""
     results = search_justice(df, text)
     print(results.head(10))
 
-
-if results.empty:
-    prompt = text
-else:
-    prompt = "Look through this information to answer the question: " + results[['text']].head(10).to_string(
-        header=False,
-        index=False).strip() + "(if it doesn't make sense you can disregard it. If you use it, please repeat the " \
-                               "information listed ). The question is: " + text
-new_content = "The question is: " + text
-messages = manage_context_length(messages, new_content)
-messages.append({"role": "user", "content": new_content})
-
-response_stream = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=messages,
-    stream=True
-)
-stopFlag = False
-for chunk in response_stream:
-    print(chunk)
-
-    if chunk.choices[0].finish_reason is None and stopFlag is False:
-        emit('response', chunk.choices[0].delta.content)
-        # TODO: Figure out how to add previous context into message messages.append(chunk.choices[0].delta)
+    if results.empty:
+        prompt = text
     else:
-        emit('stop_reason', chunk.choices[0].finish_reason)
-        stopFlag = True
+        prompt = "Look through this information to answer the question: " + results[['text']].head(10).to_string(
+            header=False,
+            index=False).strip() + "(if it doesn't make sense you can disregard it. If you use it, please repeat the " \
+                                   "information listed or give the examples in GNU Radio ). The question is: " + text
+    new_content = "The question is: " + text
+    messages.append({"role": "user", "content": new_content})
+
+    response_stream = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=manage_context_length(messages),
+        stream=True
+    )
+    stopFlag = False
+    for chunk in response_stream:
+        print(chunk)
+
+        if chunk.choices[0].finish_reason is None and stopFlag is False:
+            emit('response', chunk.choices[0].delta.content)
+        else:
+            emit('stop_reason', chunk.choices[0].finish_reason)
+            stopFlag = True
+
 
 if __name__ == "__main__":
     text = input('\r\n User: ')
