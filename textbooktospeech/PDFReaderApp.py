91aaf43edbb21224f4425f2268daf8b4479b5365
Bradlee Harrison <bradlee.harrison22@gmail.com>
2023-11-27 07:02:41 -0600

    Textbook PDF GUI

--- textbooktospeech/PDFReaderApp.py
+++ textbooktospeech/PDFReaderApp.py
@@ -23,20 +23,35 @@ class PDFReaderApp:
     def __init__(self, master, pdf_path):
         self.master = master
         self.current_page = START_PAGE
-        self.image_width = 800
-        self.image_height = 1000
+        self.image_width = 1000  # Width of the canvas
+        self.image_height = 400  # Height of the canvas
         self.pdf_path = pdf_path
         self.pages_cache = {}  # Cache for storing pre-converted pages
         self.num_cached_pages = 20
         self.pre_cache_radius = 10  # Number of pages to pre-cache around the current page
-        self.update_cache(self.current_page)
+        self.canvas_image = None  # Placeholder for the canvas image
 
         master.title("PDF Reader")
-        master.geometry(str(self.image_width) + 'x' + str(self.image_height))  # Width x Height
+        master.geometry(f'{self.image_width + 30}x{self.image_height + 100}')  # Window size
 
         threading.Thread(target=self.update_cache, args=(self.current_page,), daemon=True).start()
 
-        print("INIT")
+        # Frame for Canvas and Scrollbar
+        self.canvas_frame = tk.Frame(master)
+        self.canvas_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        # Create a Canvas with Scrollbar inside the frame
+        self.canvas = tk.Canvas(self.canvas_frame, width=self.image_width, height=self.image_height)
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        self.scrollbar = ttk.Scrollbar(self.canvas_frame, orient="vertical", command=self.canvas.yview)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.canvas.bind('<Configure>', self.on_canvas_configure)
+        self.canvas.bind('<MouseWheel>', self.on_mousewheel)  # For Windows
+        self.canvas.bind('<Button-4>', self.on_mousewheel)  # For Linux
+        self.canvas.bind('<Button-5>', self.on_mousewheel)  # For Linux
 
         # Loading Indicator
         self.loading_label = ttk.Label(master, text="Loading...", font=("Arial", 20))
@@ -46,7 +61,7 @@ class PDFReaderApp:
         self.page_number_label = ttk.Label(master, text="", font=("Arial", 12))
         self.page_number_label.pack(side=tk.BOTTOM, fill=tk.X)
 
-        # Navigation
+        # Navigation Frame
         self.nav_frame = ttk.Frame(master)
         self.nav_frame.pack(side=tk.BOTTOM, fill=tk.X)
 
@@ -59,67 +74,77 @@ class PDFReaderApp:
         self.next_button = ttk.Button(self.nav_frame, text="Next", command=self.next_page)
         self.next_button.pack(side=tk.LEFT, padx=10, pady=10)
 
-        # Image
-        self.image_label = ttk.Label(master)
-        self.image_label.pack(fill=tk.BOTH, expand=True)
         # Display the first page
         self.display_page(self.current_page)
+
+    def on_canvas_configure(self, event):
+        # Set the scroll region to encompass the image
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+    def update_image(self, photo):
+        self.canvas.delete("all")
+        self.canvas.create_image(0, 0, anchor=tk.NW, image=photo)
+        self.canvas.image = photo  # Keep a reference
+        self.loading_label.pack_forget()
+        self.canvas.configure(scrollregion=self.canvas.bbox(tk.ALL))
 
     def pre_cache_page(self, page_number):
         try:
             pages = convert_from_path(self.pdf_path, first_page=page_number + 1, last_page=page_number + 1)
             image = pages[0]
-            image_resized = image.resize((self.image_width, self.image_height), Image.Resampling.LANCZOS)
-            photo = ImageTk.PhotoImage(image_resized)
+            photo = ImageTk.PhotoImage(image)
             self.pages_cache[page_number] = photo
         except Exception as e:
             print(f"Error pre-caching page {page_number}: {e}")
 
     def display_page(self, page_number):
         if page_number in self.pages_cache:
-            self.image_label.config(image=self.pages_cache[page_number])
-            self.image_label.image = self.pages_cache[page_number]  # Keep a reference
+            self.update_image(self.pages_cache[page_number])
             self.loading_label.pack_forget()
         else:
             self.loading_label.pack(fill=tk.BOTH, expand=True)
             threading.Thread(target=self.convert_page, args=(page_number,), daemon=True).start()
         self.update_page_number_display(page_number)
-        self.update_cache(page_number)
 
     def next_page(self):
         if self.current_page < MAX_PAGES:
             self.current_page += 1
             self.display_page(self.current_page)
+            threading.Thread(target=self.update_cache, args=(self.current_page,), daemon=True).start()
 
     def prev_page(self):
         if self.current_page > 0:
             self.current_page -= 1
             self.display_page(self.current_page)
+            threading.Thread(target=self.update_cache, args=(self.current_page,), daemon=True).start()
 
     def convert_page(self, page_number):
         try:
             pages = convert_from_path(self.pdf_path, first_page=page_number + 1, last_page=page_number + 1)
             image = pages[0]
-            image_resized = image.resize((self.image_width, self.image_height), Image.Resampling.LANCZOS)
-            photo = ImageTk.PhotoImage(image_resized)
+            photo = ImageTk.PhotoImage(image)
+            self.pages_cache[page_number] = photo
             self.master.after(0, self.update_image, photo)
         except Exception as e:
             print(f"Error converting page {page_number}: {e}")
 
-    def update_image(self, photo):
-        self.image_label.config(image=photo)
-        self.image_label.image = photo
-        self.loading_label.pack_forget()
-
     def update_page_number_display(self, page_number):
-        total_pages = len(self.pages_cache)
         self.page_number_label.config(text=f"Page {page_number + 1} of {MAX_PAGES}")
 
+    def on_mousewheel(self, event):
+        """Handles mouse wheel scrolling."""
+        if event.num == 4 or event.delta > 0:  # Scroll up
+            self.canvas.yview_scroll(-1, 'units')
+        elif event.num == 5 or event.delta < 0:  # Scroll down
+            self.canvas.yview_scroll(1, 'units')
+
     def update_cache(self, current_page):
-        print("attempting to update cache")
         total_pages = MAX_PAGES
         start_page = max(0, current_page - self.pre_cache_radius)
         end_page = min(current_page + self.pre_cache_radius, total_pages)
+        print(
+            "attempting to update cache. Current page:" + str(current_page) + " Start page: " + \
+            str(start_page) + " End page: " + str(end_page))
 
         # Add new pages to the cache
         for page_number in range(start_page, end_page):
@@ -145,20 +170,20 @@ class PDFReaderApp:
         print(page_number)
         pdftext = extract_pages_with_complex_content(self.pdf_path, 'images', page_number)
         messageText = """Analyze the image as a page from a 'Probability and Random Variables' Textbook.
-                      Imagine you are reading aloud from a textbook. Your task is to describe and explain the 
-                      content in a way that is clear and understandable, especially focusing on equations, figures, and 
-                      complex topics. Assume the reader is listening to this as an audio book and does not have
-                      visual access to the content. This means you should explain figures and equations verbatim, in a way 
-                      that an audio reader might describe them. As for text, attempt to remain as close to verbatim as possible,
-                      keeping the train of thought from page-to-page. 
-                      Symbols need to be replaced with text descriptions. For example xᵢ may need to be repesented as "x sub i" 
-                      and η may need to be represented as "eta", and so on.
-                      Speak in the POV that you are the text. Do not reference "The Text" or "The Page"
-                      Please structure your response similar to the text. Throughout add useful information such as insights,
-                    summaries, and educational elaborations or useful applications.
-                    Indicate that you are switching to a new paragraph by saying "New Paragraph"
-                      Please do your best. 
-                      This is very important for my career."""
+                          Imagine you are reading aloud from a textbook. Your task is to describe and explain the 
+                          content in a way that is clear and understandable, especially focusing on equations, figures, and 
+                          complex topics. Assume the reader is listening to this as an audio book and does not have
+                          visual access to the content. This means you should explain figures and equations verbatim, in a way 
+                          that an audio reader might describe them. As for text, attempt to remain as close to verbatim as possible,
+                          keeping the train of thought from page-to-page. 
+                          Symbols need to be replaced with text descriptions. For example xᵢ may need to be repesented as "x sub i" 
+                          and η may need to be represented as "eta", and so on.
+                          Speak in the POV that you are the text. Do not reference "The Text" or "The Page"
+                          Please structure your response similar to the text. Throughout add useful information such as insights,
+                        summaries, and educational elaborations or useful applications.
+                        Indicate that you are switching to a new paragraph by saying "New Paragraph"
+                          Please do your best. 
+                          This is very important for my career."""
         client = OpenAI()
         image_path = "images/page_" + str(page_number + 1) + ".png"
         base64_image = encode_image(image_path)
